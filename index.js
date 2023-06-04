313016fe8bf4005a2b1c56192b46e33e1a8af63a
Alcantn1 <nalcantara92@gmail.com>
2023-06-03 22:21:12 -0700

    index.js

--- index.js
+++ index.js
@@ -6,32 +6,64 @@ const Intern = require('.lib/Intern.js');
 
 const team = [];
 
-function promptManger() {
+function promptManager() {
+    console.log('Please enter manager details:');
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Name:',
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'Employee ID:',
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'Email:',
+        },
+        {
+          type: 'input',
+          name: 'officeNumber',
+          message: 'Office Number:',
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        team.push(manager);
+        promptTeamMembers();
+      });
+  }
+
+function PromptTeamMembers() {
     inquirer
     .prompt([
         {
-            type:'input',
-            name: 'name',
-            message: ' Name:',
-        },
-        {
-            type:'input',
-            name: 'id',
-            message: 'Employee ID:',
+            type:'list',
+            name:'role',
+            message:'Role',
+            choices:['engineer', 'Intern'],
         }
-        {
-            type:'input',
-            name: 'email',
-            message: 'Email:',
-        }
-        {
-            type:'input',
-            name: 'officeNumber',
-            message: 'Office Number:',
-        }
-        
     ])
-}
+    .then((answers) => {
+        if (answers.role === 'Engineer') {
+          promptEngineer();
+        } else if (answers.role === 'Intern') {
+          promptIntern();
+        } else {
+          generateHTML(team);
+        }
+      });
+  }
+
 
 
 function promptEngineer() {
@@ -46,19 +78,31 @@ function promptEngineer() {
             type:'input',
             name: 'id',
             message: 'Employee ID:',
-        }
+        },
         {
             type:'input',
             name: 'email',
             message: 'Email:',
-        }
+        },
         {
             type:'input',
             name: 'github',
             message: 'Github username:',
         }
     ])
+    .then((answers) => {
+        const engineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github
+        );
+        team.push(engineer);
+        promptTeamMembers();
+        
+    });
 }
+
 
 
 function promptIntern() {
@@ -73,18 +117,29 @@ function promptIntern() {
             type:'input',
             name: 'id',
             message: 'Employee ID:',
-        }
+        },
         {
             type:'input',
             name: 'email',
             message: 'Email:',
-        }
+        },
         {
             type:'input',
             name: 'school',
             message: 'School:',
-        }
+        },
     ])
+    .then((answers) => {
+        const intern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github,
+            answers.school
+        );
+        team.push(intern);
+        PromptTeamMembers();
+});
 }
 
 
